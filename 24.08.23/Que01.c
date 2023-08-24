b242a85be8d3a40ef9d3e64c1484ab64e5ccc316
harsh7598 <harryht7@gmail.com>
2023-08-24 14:58:12 +0530

    'first'

---
+++ 24.08.23/Que01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int a = 42;
+    float b = 3.14159;
+    char c = 'A';
+
+    printf("Integer Variable: %d (Type: int)\n", a);
+    printf("Float Variable: %f (Type: float)\n", b);
+    printf("Char Variable: %c (Type: char)\n", c);
+
+    return 0;
+}
